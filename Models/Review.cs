9f6a6e8d97f7b16558f604a14d5083c72651b901
Jeff M <jeffkmanser@gmail.com>
2024-10-15 19:47:19 -0300

    created a Review model, and a Reviews model

---
+++ Models/Review.cs
@@ -0,0 +1,9 @@
+public class Review
+{
+    public int reviewID { get; set; }
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public DateTime reviewDate { get; set; }
+    public int rating { get; set; }
+    public string comment { get; set; }
+}
--- Models/Reviews.cs
+++ Models/Reviews.cs
@@ -4,12 +4,12 @@ using DotNetEnv;
 namespace projectOne.Models
 {
 
-    public class MyModel
+    public class Reviews
     {
 
         private string CONNECTION_STRING;
 
-        public MyModel()
+        public Reviews()
         {
             Env.Load();
             CONNECTION_STRING = Env.GetString("CONNECTION_STRING");
