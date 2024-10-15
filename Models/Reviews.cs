b68db83f3b3d9e2b82765a7395397a823ece90c6
Jeff M <jeffkmanser@gmail.com>
2024-10-15 19:57:31 -0300

    added mysql variables and initialized them

--- Models/Reviews.cs
+++ Models/Reviews.cs
@@ -1,5 +1,9 @@
 using System;
+using MySqlConnector;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using DotNetEnv;
+
 
 namespace projectOne.Models
 {
@@ -9,10 +13,23 @@ namespace projectOne.Models
 
         private string CONNECTION_STRING;
 
+        // db connectivity variables
+        private MySqlConnection dbConnection;
+        private MySqlCommand dbCommand;
+        private MySqlDataReader dbReader;
+
+        // property variables
+        
+
+
         public Reviews()
         {
             Env.Load();
             CONNECTION_STRING = Env.GetString("CONNECTION_STRING");
+
+            // construct required DB objects for use in private methods
+            dbConnection = new MySqlConnection(CONNECTION_STRING);
+            dbCommand = new MySqlCommand("", dbConnection);
         }
 
     }
